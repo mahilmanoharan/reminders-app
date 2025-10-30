689467099ea5566451a963d5f24ab44cf43fd5e5
Mahil <mahil@unc.edu>
2025-10-30 17:41:55 -0400

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Mahil Manoharan on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
